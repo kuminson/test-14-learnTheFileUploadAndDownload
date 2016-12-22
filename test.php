678f8c87430aa1d8b00cbbd43d4825ea8da536d0
kumin <273423480@qq.com>
2016-12-22 18:10:26 +0800

    add code

---
+++ test.php
@@ -0,0 +1,3 @@
+<?php
+echo "test-14-learnTheFileUploadAndDownload 发布成功！"
+?>
---
+++ upload.php
@@ -0,0 +1,14 @@
+<?php
+// header("content-type:html/text; charset=UTF-8");
+echo "<pre>";
+print_r($_FILES);
+
+$filename = $_FILES["myfile"]["name"];
+$type = $_FILES["myfile"]["type"];
+$tmp_name = $_FILES["myfile"]["tmp_name"];
+$error = $_FILES["myfile"]["error"];
+$size = $_FILES["myfile"]["size"];
+
+// move_uploaded_file($tmp_name,$_SERVER["DOCUMENT_ROOT"]."/file/".$filename);
+copy($tmp_name, $_SERVER["DOCUMENT_ROOT"]."/file/".$filename);
+?>
